5646c4dd3053721128eb9aec393f96aa1752d76c
Mini_Dragon <projecteurs@gmail.com>
2015-01-16 23:29:08 -0800

    Patch

--- GladiatorlosSA_zhCN/GladiatorlosSA_zhCN.lua
+++ GladiatorlosSA_zhCN/GladiatorlosSA_zhCN.lua
@@ -1,4 +1,4 @@
 ﻿
 local GSA = GladiatorlosSA
-rawset (GSA.GSA_LANGUAGE, "GladiatorlosSA_zhCN\\Voice_zhCN", "Chinese(Yike)")
+rawset (GSA.GSA_LANGUAGE, "GladiatorlosSA_zhCN\\Voice_zhCN", "Chinese(female)")
 rawset (GSA.GSA_LOCALEPATH, "zhCN", "GladiatorlosSA_zhCN\\Voice_zhCN")
